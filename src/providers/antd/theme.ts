adb5328e7d102a50a0cd8b3d99589b77e5a35de9
Rezaraki <rezakadil@gmail.com>
2023-10-30 19:07:59 +0330

    feat: header styles done

--- src/providers/antd/theme.ts
+++ src/providers/antd/theme.ts
@@ -5,6 +5,9 @@ export const darkTheme = {
   token: {
     colorBgBase: '#020b1f', // --surface--100
     colorTextBase: '#c4d4ec', // --surface--600
+    colorBorder: '#182040', // --surface--300
+    colorBgContainer: '#020B1F', // --surface--100
+    borderRadius: 16,
   },
   components: {
     Table: {
@@ -18,12 +21,17 @@ export const darkTheme = {
       controlHeight: 56,
       borderRadius: 16,
       borderRadiusSM: 16,
+      borderRadiusXS: 16,
       lineWidthBold: 6,
+    },
+    input: {
+      controlHeightLG: 48,
     },
   },
   algorithm: darkAlgorithm,
 };
 export const defaultTheme = {
+  token: { borderRadius: 16 },
   components: {
     Table: {
       headerBorderRadius: 16,
